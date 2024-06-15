f648e44111c93a65b8d5d99d91fc2903a7ea8819
KayBruh1 <kzahiri@dons.usfca.edu>
2024-06-15 15:10:44 -0700

    Work on create profile and login screens

---
+++ new-profile/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import Profile from './Profile';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Profile />
+      </header>
+    </div>
+  );
+}
+
+export default App;
