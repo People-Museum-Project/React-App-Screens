c754680313d34fafe48d769a07952b01e083c9f8
BianYi10 <bianyi10@gmail.com>
2024-07-26 22:33:52 -0700

    change my test script

--- src/utils_test.js
+++ src/utils_test.js
@@ -7,7 +7,8 @@ const {
   getPersonListByCollection, 
   addPersonCollection, 
   getCollection,
-  addCollection 
+  addCollection,
+  deletePersonFromCollection 
 } = require('./utils');
 
 const formData = {
@@ -25,7 +26,7 @@ async function test() {
     // await getUser('5639601012080640');
     // await updateUser('5651294463197184', 'John Smith', 'http://example.com/xxxxx.jpg', 'An updated user');
 
-    await getPersonList('7s6fzzlWcifsb7iS7Q8TdTOHoW72');
+    // await getPersonList('7s6fzzlWcifsb7iS7Q8TdTOHoW72');
 
     // await getPersonList(5651294463197184, 1, 10);
 
@@ -38,6 +39,7 @@ async function test() {
     // await addPersonCollection(5632139873746944, 5675594515742720);
     // await getCollection(5739062455435264);
     // await addCollection(formData);
+    await deletePersonFromCollection("5708300221939712","5751095477403648");
     
 
   } catch (error) {
